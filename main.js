926c9516b062a8ff1498e0a580bc83040f7124a2
Rebecca King <bexking@Rebeccas-MacBook-Pro-2.local>
2019-09-18 14:01:22 +1200

    First commit

---
+++ main.js
@@ -0,0 +1,12 @@
+// let translation = [];
+// translation[0]= {word:"apple" emoji: "🍎"}
+
+
+	
+//How to straight copy:
+document.getElementsByClassName('textField')[0].onchange = function(e){
+	document.getElementsByClassName('textField')[1].value =
+	document.getElementsByClassName('textField')[0].value.split();
+}
+
+
