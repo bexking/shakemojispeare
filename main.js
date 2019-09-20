e02c3e4d043bf3460fc86fbf64503e89112fb46a
Rebecca King <bexking@Rebeccas-MacBook-Pro-2.local>
2019-09-20 21:45:46 +1200

    changing from box a = box b. working on code to access the emoji dictionary array

--- main.js
+++ main.js
@@ -1,12 +1,34 @@
 // let translation = [];
 // translation[0]= {word:"apple" emoji: "🍎"}
 
-
-	
 //How to straight copy:
-document.getElementsByClassName('textField')[0].onchange = function(e){
-	document.getElementsByClassName('textField')[1].value =
-	document.getElementsByClassName('textField')[0].value.split();
+// document.getElementsByClassName('textField')[0].onchange = function(e){
+// 	document.getElementsByClassName('textField')[1].value =
+// 	document.getElementsByClassName('textField')[0].value;
+// }
+
+let dictionary = [
+	{word: "apple", emoji="🍎"}, 
+	{word: "cat", emoji="🐈"}, 
+]
+	
+let userInput = document.getElementsByClassName('textField')[0];
+let output = document.getElementsByClassName('textField')[1];	
+
+
+document.getElementsByClassName('textField')[0].onchange = function(){
+	let split=userInput.value.split(" ");
+	split.map(function(wordTranslate){
+		let translation=dictionary.filter(words => {
+			return (words.word == wordTranslate)
+		});
+		
+	})
 }
+
+// document.getElementsByClassName('textField')[0].onchange = function(e){
+// 	document.getElementsByClassName('textField')[1].value =
+// 	document.getElementsByClassName('textField')[0].value.split();
+// }
 
 
