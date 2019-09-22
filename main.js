e91dcfba0f226a6b45570563519501acf4d1726a
Rebecca King <bexking@Rebeccas-MacBook-Pro-2.local>
2019-09-22 13:18:15 +1200

    changing the functions to get the translation to print in box 2 to be less nested so I can follow it easier. Getting words with a translation to print to box 2. Adding meta tag to index so emojis still display correctly once it's gone through the function.

--- main.js
+++ main.js
@@ -8,22 +8,25 @@
 // }
 
 let dictionary = [
-	{word: "apple", emoji="🍎"}, 
-	{word: "cat", emoji="🐈"}, 
-]
+	{word: "apple", emoji: "🍎"},
+	{word: "cat", emoji: "🐈"} 
+];
 	
 let userInput = document.getElementsByClassName('textField')[0];
 let output = document.getElementsByClassName('textField')[1];	
 
+function wordTranslate(wordToTranslate){
+	let translation=dictionary.filter(words => {
+		return (words.word == wordToTranslate)
+	});
+	return translation[0].emoji;
+}
+
 
 document.getElementsByClassName('textField')[0].onchange = function(){
 	let split=userInput.value.split(" ");
-	split.map(function(wordTranslate){
-		let translation=dictionary.filter(words => {
-			return (words.word == wordTranslate)
-		});
-		
-	})
+	let asdf = split.map(wordTranslate);
+	output.value = asdf;
 }
 
 // document.getElementsByClassName('textField')[0].onchange = function(e){
